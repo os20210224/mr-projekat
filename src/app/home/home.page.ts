5b0e71dc2be82aa47cdb1d6d77e4e8ed42700957
os20210224 <os20210224@student.fon.bg.ac.rs>
2026-08-18 21:09:54 +0200

    GUI ideja

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, ViewChild } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonTabs,
+  IonTab,
+  IonTabBar,
+  IonTabButton, IonIcon, IonButton
+} from '@ionic/angular/standalone';
+import {addIcons} from "ionicons";
+import {personCircleOutline, podiumOutline, rocketOutline} from "ionicons/icons";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonTabs, IonTab, IonTabBar, IonTabButton, IonIcon, IonButton],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({
+      rocketOutline,
+      podiumOutline,
+      personCircleOutline
+    })
+  }
 }
