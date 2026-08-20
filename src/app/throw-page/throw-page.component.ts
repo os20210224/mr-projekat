fcfce2ebced20bf8f8ea84c69f90daa460f436f3
os20210224 <os20210224@student.fon.bg.ac.rs>
2026-08-20 22:02:21 +0200

    test senzora
    podaci sa akcelerometra i ziroskopa se uspeno citaju

--- src/app/throw-page/throw-page.component.ts
+++ src/app/throw-page/throw-page.component.ts
@@ -1,19 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import {IonButton, IonContent} from "@ionic/angular/standalone";
+import { CommonModule, DecimalPipe  } from '@angular/common';
+import {
+  Component,
+  OnDestroy,
+} from '@angular/core';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonText,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { Motion } from '@capacitor/motion';
 
 @Component({
   selector: 'app-throw-page',
   templateUrl: './throw-page.component.html',
   styleUrls: ['./throw-page.component.scss'],
+  standalone: true,
   imports: [
+    CommonModule,
+    DecimalPipe,
+    IonButton,
     IonContent,
-    IonButton
-  ]
+    IonHeader,
+    IonText,
+    IonTitle,
+    IonToolbar,
+  ],
 })
-export class ThrowPageComponent  implements OnInit {
+export class ThrowPageComponent {
+  sensorData: any = null;
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  async testSensors() {
+    await Motion.addListener('accel', event => {
+      this.sensorData = {
+        accelerometer: {
+          x: event.acceleration?.x,
+          y: event.acceleration?.y,
+          z: event.acceleration?.z,
+        },
+        gravity: {
+          x: event.accelerationIncludingGravity?.x,
+          y: event.accelerationIncludingGravity?.y,
+          z: event.accelerationIncludingGravity?.z,
+        },
+        gyroscope: {
+          alpha: event.rotationRate?.alpha,
+          beta: event.rotationRate?.beta,
+          gamma: event.rotationRate?.gamma,
+        },
+      };
+    });
+  }
 }
